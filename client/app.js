df46077357fddd23fc9d38f2efc9213ee680cd5c
xixilive <xixilive@gmail.com>
2020-04-23 19:35:09 +0800

    upto date

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 const {setup, http, Logger} = require('@feathers-weapp/client')
 setup('https://wis.qq.com/', {
   onLaunch(){
-    http.logger = new Logger(console)
+    http.logger = new Logger(console, 'debug', 'DEMO -> ')
   }
 })
